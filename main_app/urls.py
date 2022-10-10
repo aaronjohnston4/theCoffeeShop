914a1ddc73d29b6b66dcd1b0b85750be9e392a19
aaronjohnston4 <aaron.johnston4@gmail.com>
2022-10-10 12:37:51 -0500

    products page running

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,10 +6,9 @@ from main_app.views import Home
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
-    path('products_list/', views.ProductList.as_view(), name="products_list"),
+    path('products/', views.ProductList.as_view(), name="products_list"),
     path('create/', views.Create.as_view(), name="create"),
     path('login/', views.Login.as_view(), name="login"),
-    path('products/', views.Products.as_view(), name="products"),
     path('show/', views.Show.as_view(), name="show"),
     path('wishlist/', views.Wishlist.as_view(), name="wishlist"),
     path('cart/', views.Cart.as_view(), name="cart"),
